0b740e11e921f35ffb4b667ac001d5db960c70cb
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-11 09:33:52 +0300

    commit tree 0863cc27fdea69296134b5f6fa6367a02f06f3c8

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const error = "test"
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+
+
+   window.bestCustomer = "not bob"
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+overwriteBestCustomer();
+
+function changeLeastFavoriteCustomer() {
+    error = "no error"
+}
+changeLeastFavoriteCustomer()
